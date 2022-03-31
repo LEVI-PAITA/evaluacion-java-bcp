7b8f0a75b242152014d4ea9afb1a823ae86be15d
Levi Paita <lpaitaureta@gmail.com>
2022-03-30 19:21:21 -0500

    add solutions

---
+++ src/main/java/com/evaluacion/java/bcp/EvaluacionJavaBcpApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion.java.bcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionJavaBcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionJavaBcpApplication.class, args);
+	}
+
+}
